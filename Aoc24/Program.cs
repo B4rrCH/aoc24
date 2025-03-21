2f4bbfba40d5770fbac4a1ab8e9605a3c784afeb
Stefan Lochau <lochaus@hotmail.com>
2025-03-21 21:53:20 +0100

    remove Spectre.Console in favour of simple output

--- Aoc24/Program.cs
+++ Aoc24/Program.cs
@@ -1,66 +1,33 @@
 ﻿using Aoc24;
 using Aoc24.Solutions;
-using Spectre.Console;
 
 IEnumerable<(string, Func<Task<PartResult>>, Func<Task<PartResult>>)> solvers =
 [
-    Solvers<Day01>(),
-    Solvers<Day02>(),
-    Solvers<Day03>(),
-    Solvers<Day04>(),
-    Solvers<Day05>(),
-    Solvers<Day06>(),
-    Solvers<Day07>(),
-    Solvers<Day08>(),
-    Solvers<Day09>(),
-    Solvers<Day10>(),
-    Solvers<Day11>(),
+    Solver<Day01>(),
+    Solver<Day02>(),
+    Solver<Day03>(),
+    Solver<Day04>(),
+    Solver<Day05>(),
+    Solver<Day06>(),
+    Solver<Day07>(),
+    Solver<Day08>(),
+    Solver<Day09>(),
+    Solver<Day10>(),
+    Solver<Day11>(),
 ];
 
-var table = new Table()
-    .AddColumns(
-        "Name",
-        "Part1",
-        "[[ms]]",
-        "Part2",
-        "[[ms]]"
-    ).MinimalBorder();
-
-var progress = new Progress(AnsiConsole.Console) { RefreshRate = TimeSpan.FromMilliseconds(10) }
-    .Columns(
-        new TaskDescriptionColumn(),
-        new ProgressBarColumn(),
-        new PercentageColumn(),
-        new SpinnerColumn(Spinner.Known.Aesthetic));
-
-await progress.StartAsync(
-    async ctx =>
-    {
-        foreach (var (name, part1, part2) in solvers)
-        {
-            var task = ctx.AddTask(name, true, 2);
-            ctx.Refresh();
-            var result1 = await part1();
-            task.Value = 1;
-            ctx.Refresh();
-            var result2 = await part2();
-            task.Value = 2;
-            ctx.Refresh();
-
-            table.AddRow(
-                name,
-                result1.Result,
-                $"{result1.TimeTaken.TotalMilliseconds:0}",
-                result2.Result,
-                $"{result2.TimeTaken.TotalMilliseconds:0}");
-        }
-    });
-
-AnsiConsole.Write(table);
+foreach (var (name, part1, part2) in solvers)
+{
+    Console.WriteLine(name);
+    var result1 = await part1();
+    Console.WriteLine($"    Part1: {result1.Result,15} ({result1.TimeTaken.TotalMilliseconds:0} ms)");
+    var result2 = await part2();
+    Console.WriteLine($"    Part2: {result2.Result,15} ({result2.TimeTaken.TotalMilliseconds:0} ms)");
+}
 
 return;
 
-static (string, Func<Task<PartResult>>, Func<Task<PartResult>>) Solvers<TSolution>()
+static (string, Func<Task<PartResult>>, Func<Task<PartResult>>) Solver<TSolution>()
     where TSolution : SolutionBase, IConstructFromReader<TSolution>
 {
     return (typeof(TSolution).Name, Solution.RunPart1<TSolution>, Solution.RunPart2<TSolution>);
